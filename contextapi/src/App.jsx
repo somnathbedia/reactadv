e7e796cfbd094db8304f5c55bfdea5c508ff714c
somnathbedia <somnathbedia7@gmail.com>
2023-10-05 22:36:41 +0530

    contextapi added

---
+++ contextapi/src/App.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const CountContext = createContext(null);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <CountContext.Provider value={{ count, setCount }}>
+      {count}
+      <CounterButton />
+    </CountContext.Provider>
+  );
+}
+
+const CounterButton = () => {
+  const { count, setCount } = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrement
+      </button>
+      <Multiply />
+    </div>
+  );
+};
+
+const Multiply = () => {
+  const { count, setCount } = useContext(CountContext);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count * 2);
+        }}
+      >
+        Multiple by 2
+      </button>
+    </div>
+  );
+};
+
+export default App;
